9dfc016a93f1766d5e4c46ab77decc9af87cad95
9mui <121283017+9mui@users.noreply.github.com>
2026-06-12 14:38:16 +0500

    Update products.js

--- products.js
+++ products.js
@@ -5,7 +5,7 @@ const PRODUCTS = [
     title: "Базовая футболка",
     price: 1490,
     category: "Футболки",
-    image: "https://media.istockphoto.com/id/1185028107/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BC%D0%BE%D0%BB%D0%BE%D0%B4%D0%B0%D1%8F-%D1%81%D0%BC%D0%B5%D1%8E%D1%89%D0%B0%D1%8F%D1%81%D1%8F-%D0%B6%D0%B5%D0%BD%D1%89%D0%B8%D0%BD%D0%B0-%D1%81%D1%82%D0%BE%D1%8F%D1%89%D0%B0%D1%8F-%D1%81-%D1%80%D1%83%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2-%D0%BA%D0%B0%D1%80%D0%BC%D0%B0%D0%BD%D0%B0%D1%85-%D0%BE%D0%B4%D0%B5%D1%82%D0%B0%D1%8F-%D0%B2-%D0%BF%D1%83%D1%81%D1%82%D1%83%D1%8E-%D0%B1%D0%B5%D0%BB%D1%83%D1%8E-%D1%84%D1%83%D1%82%D0%B1%D0%BE%D0%BB%D0%BA%D1%83-%D1%81.jpg?s=2048x2048&w=is&k=20&c=OLpbfXaW9CtstuWkQQAnkJBHGl9lTw-o6CTbFG_odNE=",
+    image: "https://pin.it/2j22uRVc4",
   },
   {
     id: 2,
